52410655380107c25add84f5201af67bdce7a40c
kallangerard <36556982+kallangerard@users.noreply.github.com>
2020-09-07 13:14:39 +0800

    Changed to react-table

--- src/components/ItemsTable/ItemsTable.js
+++ src/components/ItemsTable/ItemsTable.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MaterialTable from 'material-table';
-
-const columns = [
-  { title: 'Product', field: 'product' },
-  { title: 'Quantity', field: 'quantity' },
-  { title: 'Barcode', field: 'barcode' },
-];
+import { useTable } from 'react-table';
 
 const ItemsTable = (props) => {
+  const columns = React.useMemo(
+    () => [
+      { Header: 'Product', accessor: 'product' },
+      { Header: 'Quantity', accessor: 'quantity' },
+      { Header: 'Barcode', accessor: 'barcode' },
+    ],
+    [],
+  );
+
+  const data = React.useMemo(() => [...props.items], [props.items]);
+
+  const tableInstance = useTable({ columns, data });
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
   return (
-    <MaterialTable
-      title="Items"
-      data={props.items}
-      columns={columns}
-      options={{
-        selection: true,
-      }}
-    ></MaterialTable>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
 ItemsTable.propTypes = {
-  items: PropTypes.objectOf(
+  items: PropTypes.arrayOf(
     PropTypes.shape({
       product: PropTypes.string,
       quantity: PropTypes.number,
--- src/components/ScanLog/ScanLog.js
+++ src/components/ScanLog/ScanLog.js
@@ -1,31 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MaterialTable from 'material-table';
-
-const columns = [
-  { title: 'Time', field: 'timestamp' },
-  { title: 'Barcode', field: 'barcode' },
-  { title: 'Quantity', field: 'quantity' },
-  {
-    title: 'Transaction Type',
-    field: 'transactionType',
-  },
-];
+import { useTable } from 'react-table';
 
 const ScanLog = (props) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Time',
+        accessor: 'timestamp',
+        Cell: (props) => {
+          return props.value;
+        },
+      },
+      { Header: 'Barcode', accessor: 'barcode' },
+      { Header: 'Quantity', accessor: 'quantity' },
+      {
+        Header: 'Transaction Type',
+        accessor: 'transactionType',
+      },
+    ],
+    [],
+  );
+
+  const data = React.useMemo(() => [...props.barcodeScans], [
+    props.barcodeScans,
+  ]);
+
+  const tableInstance = useTable({ columns, data });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
   return (
-    <div>
-      <MaterialTable
-        title="Barcode Log"
-        data={props.barcodeScans}
-        columns={columns}
-      />
-    </div>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
 ScanLog.propTypes = {
-  barcodeScans: PropTypes.func.isRequired,
+  barcodeScans: PropTypes.arrayOf(
+    PropTypes.shape({
+      barcode: PropTypes.string,
+      quantity: PropTypes.number,
+      transactionType: PropTypes.string,
+      timestamp: PropTypes.number,
+    }),
+  ).isRequired,
 };
 
 export default ScanLog;
